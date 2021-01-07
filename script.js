c3f9dcec51abec4c29c182c4789c12785bce30a6
raizy21 <chiper_raizy@yahoo.com>
2021-01-07 16:25:35 +0100

    init-video_player

---
+++ script.js
@@ -0,0 +1,31 @@
+/*******
+ * links
+ * {@link} - https://pixabay.com/videos/
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/CSS/calc()
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+ * {@link} - https://www.w3schools.com/tags/ref_av_dom.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+ * {@link} - https://www.w3schools.com/howto/howto_js_fullscreen.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
+ * {@link} - https://security.stackexchange.com/questions/126407/why-does-chrome-send-four-browsers-in-the-user-agent-header
+ *****/
+
+// play & pause ----------------------------------- //
+
+
+
+// progress bar ---------------------------------- //
+
+
+
+// volume controls --------------------------- //
+
+
+
+// change playback speed -------------------- //
+
+
+
+// fullscreen ------------------------------- //
+
+
